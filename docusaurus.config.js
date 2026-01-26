7bd052345b7710149c8ba4f05efad33bddc0ccbe
Keluashi <keluashikeloni@gmail.com>
2026-01-26 07:12:11 +0300

    Начало начал

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -23,7 +23,7 @@ const config = {
   url: 'https://thesleepingbyakko.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/CobuildingWiki/',
   trailingSlash: false,
 
   // GitHub pages deployment config.
