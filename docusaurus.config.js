3ffd8829a13120c75aefd4be274832033b9b2600
Keluashi <keluashikeloni@gmail.com>
2026-01-26 06:58:38 +0300

    changes for gh-pages

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,15 +20,16 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://thesleepingbyakko.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
+  trailingSlash: false,
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'thesleepingbyakko', // Usually your GitHub org/user name.
+  projectName: 'CobuildingWiki', // Usually your repo name.
 
   onBrokenLinks: 'throw',
 
